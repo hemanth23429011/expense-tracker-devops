0a845115388905e52c41e0a1a00f02116667a99d
hemanth <x23429011@student.ncirl.ie>
2025-04-05 21:19:04 +0530

    test case

--- expenses/tests.py
+++ expenses/tests.py
@@ -1,22 +1,7 @@
 from django.test import TestCase
-from datetime import date
-from expenses.models import Expense  # Replace 'expenses' with your app name
+from django.urls import reverse
 
-class ExpenseModelTest(TestCase):
-
-    def setUp(self):
-        self.expense = Expense.objects.create(
-            title="Grocery Shopping",
-            amount=120.50,
-            category="Food",
-            date=date(2024, 4, 5)
-        )
-
-    def test_expense_creation(self):
-        self.assertEqual(self.expense.title, "Grocery Shopping")
-        self.assertEqual(self.expense.amount, 120.50)
-        self.assertEqual(self.expense.category, "Food")
-        self.assertEqual(self.expense.date, date(2024, 4, 5))
-
-    def test_expense_str_method(self):
-        self.assertEqual(str(self.expense), "Grocery Shopping - 120.50")
+class URLTest(TestCase):
+    def test_url_exists(self):
+        response = self.client.get(reverse('expense_list'))
+        self.assertEqual(response.status_code, 200)
--- expensetracker/settings.py
+++ expensetracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&m@)+1#e7@9gnq7bq0x)job!&*5*m(w(5pf=p8#1dm&uu9b-%g"
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
