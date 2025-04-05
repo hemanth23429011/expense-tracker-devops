a068466bc4bd7a9da71c02eaeff45656075dc420
hemanth <x23429011@student.ncirl.ie>
2025-04-05 21:35:54 +0530

    Update tests.py

--- expenses/tests.py
+++ expenses/tests.py
@@ -1,7 +1,3 @@
 from django.test import TestCase
-from django.urls import reverse
 
-class URLTest(TestCase):
-    def test_url_exists(self):
-        response = self.client.get(reverse('add_expense'))
-        self.assertEqual(response.status_code, 200)
+# Create your tests here.
